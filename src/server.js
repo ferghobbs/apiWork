278d8c11f74fca0f27649a1a5c8b26fd54477499
Your Name <fgomezhobbs@gmail.com>
2020-11-04 18:23:18 -0300

    Agregue post peticionesCallpicker

--- src/server.js
+++ src/server.js
@@ -2,12 +2,20 @@ import express from 'express'
 
 const app = express();
 
+app.use(express.json());
 //setings
 
 app.set('port', process.env.PORT ||3000 );
 
 app.get("/", (req,res)=> res.send("Hello world"));
 
-app.get('/dale', (req,res)=> res.send("Dale"))
+app.get('/dale', (req,res)=> res.send("Dale"));
+
+app.post('/peticionCallpicker', function(request, response) {
+
+    console.log(request.body);      // your JSON
+    response.send(request.body.name); 
+    
+});
 
  export default app;
