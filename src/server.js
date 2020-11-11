c5b53bdf7a726e4fead786a0d15b4149d48f0157
Your Name <fgomezhobbs@gmail.com>
2020-11-11 14:12:05 -0300

    Se agregaron todas las sucursales

--- src/server.js
+++ src/server.js
@@ -52,4 +52,142 @@ app.post('/BocaDelRio', function (req, res, next) {
   tawkto.cargarTawkto(req,res,WEBHOOK_SECRET2,sucursal2)
 });
 
+const WEBHOOK_SECRET3 = '6c0cea88dec4706d509596ab7c2c3f7c053984c24d340014f0662d72ec6934a59932ecc7a2f0126d535b621a74f1a751';
+const sucursal3 = 'Interlomas'
+
+app.post('/Interlomas', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET3,sucursal3)
+});
+
+const WEBHOOK_SECRET4 = '27a8885c806c8e1d1602fa7a1cdb6371c6684feb02f7655119aede597a661f35980a77732d9e503df7e8c9d738be8a43';
+const sucursal4 = 'Palmas'
+
+app.post('/Palmas', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET4,sucursal4)
+});
+
+const WEBHOOK_SECRET5 = '2e22660d5b6e5b934d0ecd6d6b6ace8b345a31e3ed263d281d1af646111bf7f31050a68135384eec223637c58fc130c9';
+const sucursal5 = 'Roma norte'
+
+app.post('/RomaNorte', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET5,sucursal5)
+});
+
+const WEBHOOK_SECRET6 = 'cfa073430de9b3ecbf290c793aca672a1be8d0987ccadbb6d3e8f626bdf7c357b7e93dedde4af877ee9575b8836300c6';
+const sucursal6 = 'Santa Fe'
+
+app.post('/SantaFe', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET6,sucursal6)
+});
+
+const WEBHOOK_SECRET6 = '0bbdd67189710332775e537bbd936a99f658a9c823e96d5a40b249a9f3a7df7727e4a8b1c28c5286cb4800ebc2f4e3a8';
+const sucursal6 = 'Lindavista'
+
+app.post('/Lindavista', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET6,sucursal6)
+});
+
+const WEBHOOK_SECRET6 = 'cf9ed82c93f42a870860554f990ebd727958a4cc60ce5bcfee1be59f6fa3a35a360a54793a417d502cea7e080a1aecc5';
+const sucursal6 = 'Narvarte'
+
+app.post('/Narvarte', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET6,sucursal6)
+});
+
+const WEBHOOK_SECRET7 = 'b162968c136e56eb1888bfd057add6515b611bb757fdd57ad9830090c63261fb3398a4288f61811359b0715ed13e0ebb';
+const sucursal7 = 'Santa Fe'
+
+app.post('/SantaFe2', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET7,sucursal7)
+});
+
+const WEBHOOK_SECRET8 = '4207aaaad031773f5366b72f63b8d65bf466a8123b34e30239dca36f456a28e995955ad7e466e11caf6ce88d07a0eefd';
+const sucursal8 = 'Mixcoac'
+
+app.post('/Mixcoac', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET8,sucursal8)
+});
+
+const WEBHOOK_SECRET10 = '6c763003b10d57e28ff8857cacbf1a120821f24d0afa8b51ac2a17844fe722518bda783d56f79c018225261dc1df7645';
+const sucursal10 = 'Roma'
+
+app.post('/Roma', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET10,sucursal10)
+});
+
+const WEBHOOK_SECRET11 = '959d53c1036bc0fca573a7cdc4839dff85ea2e5c5aab846a7a96a12a63248b0c9f69ff2d3f50847d6eec4bc51fa1d208';
+const sucursal11 = 'Principal'
+
+app.post('/Principal', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET11,sucursal11)
+});
+
+const WEBHOOK_SECRET12 = '7c8bf9e90589e43fc1572f304592a0807ffc8ea213923885dab6da301fd89838955c9f85a606d78258bcb5f84f5a76fa';
+const sucursal12 = 'Chapultepec'
+
+app.post('/Chapultepek', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET12,sucursal12)
+});
+
+const WEBHOOK_SECRET13 = 'a45615b285421f961c87b75c1d5cf1cc3b9c0a388ea05463b2a238dac85cdea3c0cb931f571c19ed6612b6260cb4c868';
+const sucursal13 = 'Guadalupe Inn'
+
+app.post('/GuadalupeInn', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET13,sucursal13)
+});
+
+const WEBHOOK_SECRET14 = '596b4a09ca8a61ac964ed13bbc19f3b1c732967ea279c2e3f30d6d14f4b9ba12c93fe09da2f7795609ff7bc5f0f146d9';
+const sucursal14 = 'Leon'
+
+app.post('/Leon', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET14,sucursal14)
+});
+
+const WEBHOOK_SECRET15 = 'a8f2f557825335cc3b88701ac7cbc10b12333d8a928b36e5f1f0a85dbd22bffb440b9d50446d3127c63b731b02262acc';
+const sucursal15 = 'Org'
+
+app.post('/Org', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET15,sucursal15)
+});
+
+const WEBHOOK_SECRET16 = '5d2cc6ed13b27a079f03eb49b6a1442510a62f4c81f9de0b8ce7f319d7c7912e14efc3acda17e6aafd64a4f514dd1a04';
+const sucursal16 = 'Tecamachalco'
+
+app.post('/Tecamachalco', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET16,sucursal16)
+});
+
+const WEBHOOK_SECRET17 = 'ed3ec3473425f38999634867812f0b67bf2a7546b16e1f18cc7247fb4ce79e86313143dea74a61a5dd6272fd18b7dcde';
+const sucursal17 = 'Satelite'
+
+app.post('/Satelite', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET17,sucursal17)
+});
+
+const WEBHOOK_SECRET18 = '70ffaebc3e0532836192164d8f1503a6ed7d3361e86943bf8b6125137bddd79a0d3bba1890104846904fa0fb32d10954';
+const sucursal18 = 'Sonrisas'
+
+app.post('/Sonrisas', function (req, res, next) {
+
+  tawkto.cargarTawkto(req,res,WEBHOOK_SECRET18,sucursal18)
+});
+
+
+
  export default app;
