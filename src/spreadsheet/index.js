cb8de5068c476bc85dceb424188c307e1fd7d2cd
Your Name <fgomezhobbs@gmail.com>
2020-12-17 17:11:33 -0300

    Arregle error de date

--- src/spreadsheet/index.js
+++ src/spreadsheet/index.js
@@ -159,7 +159,7 @@ function formatDate(date) {
 
   if (month.length < 2) 
       month = '0' + month;
-  if (day.length < 3) 
+  if (day.length < 2) 
       day = '0' + day;
 
   return [year, month, day].join('-');
